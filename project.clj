461dfbfb65f5cbb799498847af2861c43173a357
Mike Hewitt <matroxmike@icloud.com>
2017-05-21 14:23:44 +1000

    reviving old project

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject genClassTest "1.0.2-SNAPSHOT"
+(defproject genClassTest "1.0.3-SNAPSHOT"
   :description "gen class test as per http://clojure.org/reference/compilation"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   
   :main genClassTest.core
   :aot :all
   :omit-source true
-
+;; super basic example - worth keeping
   )
--- src/genClassTest/core.clj
+++ src/genClassTest/core.clj
@@ -10,3 +10,4 @@
   )
 
 ;; this could help my sudoku quest {3 input words} - I obviously need to read up on some stuff here
+;; could make more useful by detecting that the wrong number of arguments have been supplied and not crash
